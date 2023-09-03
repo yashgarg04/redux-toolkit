e7b4fa130a7c5a830f08ad906f0852f037fc8a0f
Yash <yashgarg0408@gmail.com>
2023-09-04 04:42:43 +0530

    final commit with async API reducer

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,20 +2,37 @@ import { useState, useEffect } from "react";
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../store/cartSlice";
+import { getProducts } from "../store/productSlice";
 
 const Product = () => {
     const dispatch = useDispatch();
 
-    const [products, getProducts] = useState([]);
+    const {data: products, status} = useSelector(state => state.products);
+
+    console.log(products);
+
+    // const [products, getProducts] = useState([]);
 
     useEffect(() => {
         //api
-        fetch('https://fakestoreapi.com/products')
-        .then(data => data.json())
-        .then(result => getProducts(result))
+        // fetch('https://fakestoreapi.com/products')
+        // .then(data => data.json())
+        // .then(result => getProducts(result))
+
+
+        //dispatch an action for fetchProducts
+         dispatch(getProducts());
     }, []);
+
+    if (status === 'loading') {
+        return <p>Loading...</p>
+    }
+
+    if (status === 'error') {
+        return <p>Something went wrong! Try again later...</p>
+    }
 
     const addToCart = (product) => {
         // dispatch an add action
@@ -24,8 +41,8 @@ const Product = () => {
     }
 
     const cards = products.map(product => (
-        <div className="col-md-3 mb-3">
-            <Card key={product.id} className="h-100">
+        <div key={product.id} className="col-md-3 mb-3">
+            <Card className="h-100">
                 <div className="text-center">
                     <Card.Img variant="top" src={product.image} style={{width: '100px', height: '130px'}} />
                 </div>
